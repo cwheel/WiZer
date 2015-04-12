c4e48bc11415534aad3a93900d7487c1249a93d3
nosedive25 <nosedive25@git.git>
2015-04-12 08:45:56 -0400

    Live dashboard

--- public/controllers/dashboardController.js
+++ public/controllers/dashboardController.js
@@ -6,36 +6,43 @@ wizer.controller('dashboardController',['$scope', '$location', '$timeout', '$roo
 	$scope.nodes = "";
 	$scope.nets = "";
 
-	$http({
-	    method : 'GET',
-	    url : '/node/recentReports'
-	})
-	.success(function(resp) {
-		if (angular.fromJson(resp).length != 0) {
-			$scope.allNetworks = angular.fromJson(resp);
+	$scope.refresh = function () {
+		console.log("fdsfs");
+		$http({
+		    method : 'GET',
+		    url : '/node/recentReports'
+		})
+		.success(function(resp) {
+			if (angular.fromJson(resp).length != 0) {
+				$scope.allNetworks = angular.fromJson(resp);
 
-			$scope.gainLow = $scope.allNetworks[0].signal;
-			$scope.gainHigh = $scope.allNetworks[0].signal;
-			$scope.nets = $scope.allNetworks.length;
+				$scope.gainLow = $scope.allNetworks[0].signal;
+				$scope.gainHigh = $scope.allNetworks[0].signal;
+				$scope.nets = $scope.allNetworks.length;
 
-			$http({
-			    method : 'GET',
-			    url : '/node/count'
-			})
-			.success(function(count) {
-				$scope.nodes = angular.fromJson(count).count;
-			});
+				$http({
+				    method : 'GET',
+				    url : '/node/count'
+				})
+				.success(function(count) {
+					$scope.nodes = angular.fromJson(count).count;
+				});
 
-			for (var i = 0; i < $scope.allNetworks.length; i++) {
-				$scope.allNetworks[i].signal = String(Math.abs(parseInt($scope.allNetworks[i].signal)) * -1) + " dBm";
+				for (var i = 0; i < $scope.allNetworks.length; i++) {
+					$scope.allNetworks[i].signal = String(Math.abs(parseInt($scope.allNetworks[i].signal)) * -1) + " dBm";
 
-				if (parseInt($scope.gainLow) <  parseInt($scope.allNetworks[i].signal)) {
-					$scope.gainLow = $scope.allNetworks[i].signal;
-				}
-				if (parseInt($scope.gainHigh) > parseInt($scope.allNetworks[i].signal)) {
-					$scope.gainHigh = $scope.allNetworks[i].signal;
+					if (parseInt($scope.gainLow) <  parseInt($scope.allNetworks[i].signal)) {
+						$scope.gainLow = $scope.allNetworks[i].signal;
+					}
+					if (parseInt($scope.gainHigh) > parseInt($scope.allNetworks[i].signal)) {
+						$scope.gainHigh = $scope.allNetworks[i].signal;
+					}
 				}
 			}
-		}
-	});
+		});
+
+		$timeout(function() {$scope.refresh()}, 2000);
+	};
+
+	$scope.refresh();
 }]);
