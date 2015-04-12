e625c7abda08b0f00c250ce015cc37a96fb57fc1
nosedive25 <nosedive25@git.git>
2015-04-12 00:22:47 -0400

    Started showing reports

--- public/controllers/dashboardController.js
+++ public/controllers/dashboardController.js
@@ -1,3 +1,3 @@
 wizer.controller('dashboardController',['$scope', '$location', '$timeout', '$rootScope', '$http', function($scope, $location, $timeout, $rootScope, $http) {
-$scope.$emit('loginCompleted', null);
+	$scope.$emit('loginCompleted', null);
 }]);
